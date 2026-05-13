960b05db8ffa244bec7c5bdd60051d5efa2bbe64
LockBall <johnplutz@gmail.com.com>
2026-05-13 15:37:31 -0400

    minor tweaks, added another mapped folder

--- config_me.bat
+++ config_me.bat
@@ -1,32 +1,26 @@
 @echo off
-echo Doing All The Things ...
+echo Configuring Windows Sandbox...
 
-    echo disable hide file extensions
-        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
+echo Show file extensions
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0 /f
 
-    echo hot and black
-        :: Remove wallpaper path
-        reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+echo Use a black desktop background
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "" /f
+reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v BackgroundType /t REG_DWORD /d 1 /f
 
-        :: Set background color to black (RGB 0 0 0)
-        reg add "HKCU\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
+echo Enable dark mode for apps and system
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
 
-        :: Optional: force solid color mode
-        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Wallpapers" /v BackgroundType /t REG_DWORD /d 1 /f
+echo Restart Explorer
+taskkill /f /im explorer.exe >nul 2>&1
+timeout /t 2 >nul
+start explorer.exe
 
-        :: Enable dark mode for apps and system
-        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
-        reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
-
-
-    echo restart explorer
-        taskkill /f /im explorer.exe
-        timeout /t 2 >nul
-        start explorer.exe
-
-    echo Reopen Explorer
-        timeout /t 1 >nul
-        start "" "C:\from_host"
+echo Open mapped host folder
+timeout /t 1 >nul
+start "" "C:\from_host"
 
 :: Keep window open
 echo.
